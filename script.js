c98327daea4511d5da70fffb4ea387d10657377b
peter199741 <117526581+peter199741@users.noreply.github.com>
2023-01-07 15:36:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,46 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  var b = arr.map(dev);
+  function dev(x){
+    if(x.profession=="developer"){
+      console.log( x.id + " " + " " + x.name +" "+ x.age + " " + x.profession ) ;
+    }
+  }
+  
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(function(value){
+    if(value.profession=="developer")
+    console.log(value);
+  })
 }
 
 function addData() {
   //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"});
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  arr.forEach(function(value){
+    if(value.profession!="admin")
+    console.log(value);
+  })
+  
 }
 
 function concatenateArray() {
   //Write your code here
+  var brr = [
+    { id: 4, name: "peter", age: "50", profession: "intern" },
+    { id: 5, name: "robin", age: "78", profession: "developer" },
+    { id: 6, name: "bruce", age: "99", profession: "admin" },
+  ];
+
+  var crr = arr.concat(brr);
+  console.log(crr);
 }
